469f3f4b01f27033b7be4b0c3f16fa7640498133
Pichetphong <pichetphong.k@gmail.com>
2025-01-31 20:56:09 +0700

    home-page

---
+++ app/provider.jsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from './theme-provider';
+
+export default function Provider({ children } ){
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
+}
---
+++ components/navBar/DropdownListMenu.jsx
@@ -0,0 +1,43 @@
+import { AlignLeft, CircleUserRound } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import UserIcon from './UserIcon';
+import Link from 'next/link';
+import { links } from '@/utils/links';
+
+const DropdownListMenu = () => {
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>
+            <AlignLeft />
+            <UserIcon />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {links.map((item, index) => {
+            return (
+              <DropdownMenuItem key={index} asChild className="cursor-pointer">
+                <Link href={item.href}>{item.label}</Link>
+              </DropdownMenuItem>
+            );
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {/* <AlignLeft />
+      <CircleUserRound /> */}
+    </>
+  );
+};
+export default DropdownListMenu;
